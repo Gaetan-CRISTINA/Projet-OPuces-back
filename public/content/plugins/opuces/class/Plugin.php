ab7d52a15b05de38c4a9be1c9c64d4ad3ab84b32
yvesropartz <yves.ropartz@gmail.com>
2022-01-24 10:17:50 +0100

    correction role moderateur

--- public/content/plugins/opuces/class/Plugin.php
+++ public/content/plugins/opuces/class/Plugin.php
@@ -516,16 +516,16 @@ class Plugin
             'Moderateur',
             // liste des autorisatrions
             [
-                'delete_chief' => false,
-                'delete_others_chief' => false,
-                'delete_private_chief' => false,
-                'delete_published_chief' => false,
-                'edit_chief' => true,
-                'edit_others_chief' => false,
-                'edit_private_chief' => false,
-                'edit_published_chief' => true,
-                'publish_chief' => false,
-                'read_private_chief' => false,
+                'delete_moderator' => false,
+                'delete_others_moderator' => false,
+                'delete_private_moderator' => false,
+                'delete_published_moderator' => false,
+                'edit_moderator' => true,
+                'edit_others_moderator' => false,
+                'edit_private_moderator' => false,
+                'edit_published_moderator' => true,
+                'publish_moderator' => false,
+                'read_private_moderator' => false,
             ]
         );
     }
