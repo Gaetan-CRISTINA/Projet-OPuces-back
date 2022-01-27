64fea052ef6a7eed74b281f9e0b4ef0d4e17d835
yvesropartz <yves.ropartz@gmail.com>
2022-01-27 10:45:28 +0100

    remise a niveau de develop

--- public/content/plugins/opuces/class/API.php
+++ public/content/plugins/opuces/class/API.php
@@ -20,6 +20,16 @@ class Api {
         $this->baseURI = dirname($_SERVER['SCRIPT_NAME']);
 
         register_rest_route(
+
+            'opuces/v1', // API Name
+            'create-custom-taxonomy', // name of route
+            [
+                'methods' => 'post',
+                'callback' => [$this, 'createCustomTaxonomy'],
+                'permission_callback' => '__return_true',
+            ]
+        );
+        register_rest_route(
             'opuces/v1', // nom de l'API
             'save-classified',
             [
@@ -88,6 +98,7 @@ class Api {
                 'permission_callback' => '__return_true',
             ]
         );
+       
         register_rest_route(
             'opuces/v1',
             'user-table',
@@ -97,7 +108,6 @@ class Api {
                 'permission_callback' => '__return_true',
             ]
         );
-
     }
       /**
        * crudUserTable
@@ -197,12 +207,55 @@ class Api {
                 'role' => 'user',
                 // 'image' => $image,
             ];
-        } else {
+        } else 
+        {
             return [ 'success' => false,
                 'error' => $createUserResult
             ];
-            }
-     }
+        }
+    }
+  /**
+       * createCustomTaxonomy
+       *  create & update post classified
+       * $request: {icategory,"name","parentCategory","description"
+       * return: succes = creation/modif
+       * 
+	   */  
+    public function createCustomTaxonomy(WP_REST_Request $request)
+    {
+        // retrieving what has been sent to the api on the endpoint /opuces/v1/create-custom-taxonomy in POST
+        $idcategory = $request->get_param('idcategory');
+        $name = $request->get_param('name');
+        $parentCategory = $request->get_param('parentCategory');
+        $description = $request->get_param('description');
+
+        //adding parentcategory
+        $term_id = get_term_by('name', $parentCategory, 'ProductCategory');
+        $categoryIdParent = $term_id->term_id;
+
+        $args = [
+            'description' => $description,
+            'slug' => '',
+            'parent' => $categoryIdParent
+        ];
+        //inserting the new custom taxonomy in database
+        $createCustomTaxonomyResult = wp_insert_term(
+            $name,
+            $idcategory,
+            $args
+        );
+        if (!is_wp_error($createCustomTaxonomyResult)) {
+            $success = true;
+        }
+        else {
+        $success = false;
+        }
+
+        return 
+        [
+        'success' => $success,
+        ];
+    }
       /**
        * saveClassified
        *  create & update post classified
--- public/content/plugins/opuces/class/Plugin.php
+++ public/content/plugins/opuces/class/Plugin.php
@@ -128,6 +128,9 @@ class Plugin
                 'show_in_rest' => true //rendre accessible avec API Wordpress
             ]
         ); 
+                // creation des custum fields attaches a classified classifiedPrice
+                add_post_meta(1 , "classifiedBuyerId" , 1 , $unique = true);
+                add_post_meta(1 , "classifiedPrice" , 1 , $unique = true);
     }
        
     // taxonomie pour l'état du produit à la vente
@@ -497,7 +500,7 @@ class Plugin
         $this->addCapAdmin(['classified']);
         $this->registerUserRole();
         $this->registerModerateurRole();
-
+        $this->registerPostStatus();
     }
     public function registerUserRole()
     {
@@ -545,13 +548,47 @@ class Plugin
         );
     }
     
+    public function registerPostStatus()
+    {
+        register_post_status(
+            // identifiant du status 
+            'notValidate',
+            [
+            'label' => 'A validé',
+            'exclude_from_search' => true,
+            'public' => false,
+            'publicly_queryable' => false,
+            'show_in_admin_status_list' => true,
+            'show_in_admin_all_list' => true,
+            'label_count'=> _n_noop( 'A valider <span class="count">(%s)</span>', 'A validé <span class="count">(%s)</span>' ),
+            ]
+        );
+    }
     /**
      * Method to desactivate Plugin
      * 
      */
     public function deactivate()
     {
+        // purge des taxo
+        $arrayTaxos = [ "ProductState","SellerRate","ProductCategory","DeliveryMethod"];
 
+        foreach ($arrayTaxos as $taxo) 
+            {
+            $term_args = array(
+            'taxonomy' => $taxo,
+            'hide_empty' => false,                
+            'orderby' => 'name',                
+            'order' => 'ASC'                
+        );
+        
+    $terms = get_terms($term_args);
+
+    foreach ($terms as $term) 
+    {
+        wp_delete_term($term->term_id, $taxo);
+    }
+}
     }
 
     public function addCapAdmin($customCapArray)
