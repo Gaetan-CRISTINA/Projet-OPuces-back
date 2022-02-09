5f59942ea279d315ce730e4c28b1b0f0dabd65a2
Etudiant O'clock <gaetan.cristina@gmail.com>
2022-02-09 14:38:11 +0100

    PRIMARY KEY SEUL

--- public/content/plugins/opuces/class/Plugin.php
+++ public/content/plugins/opuces/class/Plugin.php
@@ -93,7 +93,8 @@ class Plugin
                 rate tinyint(1) ,
                 created_at datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
                 updated_at datetime NULL,
-                userID bigint(20) unsigned NOT NULL PRIMARY KEY);
+                userID bigint(20) unsigned NOT NULL,
+                PRIMARY KEY (userID));
             ";
                 // wp_users_id bigint(24),
                 // FOREIGN KEY(wp_users_id) REFERENCES wp_users(id)
