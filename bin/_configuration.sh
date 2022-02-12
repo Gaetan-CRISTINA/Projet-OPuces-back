4e78ae71548ef24640a4ce854d858b081a9784f5
Etudiant O'clock <gaetan.cristina@gmail.com>
2022-02-12 14:19:18 +0100

    script insall +wp

---
+++ bin/_configuration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MYSQL_USER="explorateur"
+MYSQL_PASSWORD="Ereul9Aeng"
+MYSQL_HOST="localhost"
+WORDPRESS_BDD="opuces"
+
+WORDPRESS_URL="http://localhost/Opuces/projet-O-Puces-back/public"
+
+WORDPRESS_SITE_NAME="opuces"
+WORDPRESS_ADMIN_NAME="gaetan"
+WORDPRESS_ADMIN_PASSWORD="gaetan"
+WORDPRESS_ADMIN_EMAIL="gaetan.cristina@gmail.com"
+
+# optionnal configurations
+WORDPRESS_FOLDER="wp"
+WORDPRESS_CONTENT_FOLDER="content"
+WORDPRESS_TABLE_PREFIX="wp_"
